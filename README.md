ce64556bfca1c193ee1a597e5b9e0c018585468e
Justin Heyes-Jones <justinhj@gmail.com>
2021-12-14 21:33:03 -0800

    solution to 1 and 2

--- README.md
+++ README.md
@@ -2,3 +2,22 @@
 
 https://adventofcode.com/2021/day/7
 
+To brute force this consider that the target position must be between the min and max.
+
+Proof:
+
+If the numbers are 10,11,12
+Clearly if the cheapest number was 9 (cost 1 + 2 + 3) everything had to move past 10 except 
+10 which was already there. Similar proof for the max.
+
+This is also a linear complexity so should be easy to brute force it.
+
+Possible optimizations...
+
+Iterative solutions that converge on a solution such as linear regression would likely fall into local minimums.
+
+There may be ways to get the solution faster with an educated starting point such as mean or median and then work in both directions.
+
+Part 2 just required a quick way to get sum of 1 .. n
+
+n = n(n+1) / 2
--- src/main/kotlin/org/justinhj/Day7.kt
+++ src/main/kotlin/org/justinhj/Day7.kt
@@ -1,7 +1,9 @@
 package org.justinhj
 
+import kotlin.math.abs
+
 fun load(filename: String): List<Int> {
-    val res = {}::class.java.getResource(filename)
+    val res = {}::class.java.getResource(filename)!!
     val content = res.readText()
     return content.lines().flatMap {
         line ->
@@ -9,10 +11,36 @@ fun load(filename: String): List<Int> {
     }
 }
 
+// Part 1
+
+fun cost(input: List<Int>, moveTo: Int): Int {
+    return input.sumOf {
+        abs(it - moveTo)
+    }
+}
+
 fun solve(input: List<Int>): Int {
+    val costs = (input.indices).map { Pair(it, cost(input, it)) }
+    val minimum = costs.minByOrNull { it.second }
+    return minimum?.second ?: 0
+}
 
+// Part 2 we provide a different cost function
 
-    return 0
+// cost is now 1 + 2 + 3 + 4 + .. + n
+// which can be calculated as n = n(n+1) / 2
+
+fun cost2(input: List<Int>, moveTo: Int): Int {
+    return input.sumOf {
+        val n = abs(it - moveTo)
+        (n * (n+1)) / 2
+    }
+}
+
+fun solve2(input: List<Int>): Int {
+    val costs = (input.indices).map { Pair(it, cost2(input, it)) }
+    val minimum = costs.minByOrNull { it.second }
+    return minimum?.second ?: 0
 }
 
 fun main() {
@@ -20,5 +48,9 @@ fun main() {
     val exampleData = load("example.txt")
     val day7Data = load("day7.txt")
 
-    println("${exampleData.size} ${day7Data.size}")
+    println(solve(exampleData))
+    println(solve(day7Data))
+
+    println(solve2(exampleData))
+    println(solve2(day7Data))
 }
